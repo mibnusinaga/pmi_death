f48c3eed0395457e9abdae2c616d4d596753b576
M Ibnu Sinaga <104486433+mibnusinaga@users.noreply.github.com>
2022-10-26 00:59:11 +0700

    Update tppo_tenggelam.py

--- tppo_tenggelam.py
+++ tppo_tenggelam.py
@@ -6,27 +6,52 @@ from folium import FeatureGroup, LayerControl, Map, Marker
 from  streamlit_folium import folium_static, st_folium
 import streamlit as st
 from folium.features import DivIcon
+from folium.plugins import FloatImage
+
 
 st.set_page_config(layout = 'wide')
-
 
 # import dataset 
 df = pd.read_csv('data/tppo_tenggelam.csv', sep = ';')
 
-
-
-
 #create folium object
 
-my_map = folium.Map(
-    location=[3.1651958, 96.8204231],
-    zoom_start=6,
-    zoom_control=False,
-    tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', 
-    attr='Esri.WorldImagery', 
-)
+my_map = folium.Map(location=[3.1651958, 96.4204231],
+                    zoom_start=6,  zoom_control=False)
+
+folium.TileLayer(
+        tiles='https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png',
+        #tiles='https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.png',
+        #name='Stamen.TonerBackground',
+        attr='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        #attr='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        subdomains='abcd'
+    ).add_to(my_map)
+
+# grafik
+folium.map.Marker(
+    [2.200068, 86.244171],
+    icon=DivIcon(
+        icon_size=(100,200),
+        icon_anchor=(0,0),
+        html=
+                '''<p style="font-family:Georgia Pro Regular;font-size:10px;color:white;">
+                Grafik jumlah PMI meninggal per tahun (2008-2022).
+                </p><br>'''
+        )
+    ).add_to(my_map)
+folium.map.Marker(
+    [3.233068, 86.344171],
+    icon=DivIcon(
+        icon_size=(650,200),
+        icon_anchor=(0,0),
+        html='''<img src="https://github.com/mibnusinaga/pmi_death/blob/main/data/pmi_tenggelam.png?raw=true"style="width:560px;height:200px;"><br>'''
+        )
+    ).add_to(my_map)
 
 
+
+# text jumlah korban meninggal
 folium.map.Marker(
     [5.533068, 103.644171],
     icon=DivIcon(
@@ -54,100 +79,44 @@ folium.map.Marker(
         )
     ).add_to(my_map)
 
+
+# text judul paragraf dan paragraf
 folium.map.Marker(
-    [7.333068, 86.844171],
+    [7.333068, 86.244171],
     icon=DivIcon(
         icon_size=(450,200),
         icon_anchor=(0,0),
-        html='<h3 style="font-family:Georgia Pro Regular">Kematian Pekerja Migran Ilegal Indonesia di Selat Malaka : Segitiga Bermudanya PMI</h3>'
-        )
-    ).add_to(my_map)
-
-folium.map.Marker(
-    [7.433068, 105.844171],
-    icon=DivIcon(
-        icon_size=(380,200),
-        icon_anchor=(0,0),
-        html=
-                '''<p style="font-family:Georgia Pro Regular;color:silver;font-size:9px;">
-                Oleh : M Ibnu Sinaga
-                </p><br>'''
+        html='<h3 style="font-family:Georgia Pro Regular;color:white;">Kematian Pekerja Migran Ilegal Indonesia di Selat Malaka : Segitiga Bermudanya PMI</h3>'
         )
     ).add_to(my_map)
 folium.map.Marker(
-    [7.233068, 105.544171],
+    [5.533068, 86.244171],
     icon=DivIcon(
-        icon_size=(380,200),
+        icon_size=(650,200),
         icon_anchor=(0,0),
-        html=
-                '''<p style="font-family:Georgia Pro Regular;color:silver;font-size:9px;">
-                mibnusinaga@gmail.com
-                </p><br>'''
+        html='<p style="font-family:Georgia Pro Regular;color:white;font-size:10px;">Oleh : </hp>'+"<a href="+"https://www.linkedin.com/in/mibnusinaga/"+">M Ibnu Sinaga</a>"
         )
     ).add_to(my_map)
 folium.map.Marker(
-    [7.000008, 105.744171],
+    [5.200068, 86.244171],
     icon=DivIcon(
-        icon_size=(380,200),
+        icon_size=(400,200),
         icon_anchor=(0,0),
         html=
-                '''<p style="font-family:Georgia Pro Regular;color:silver;font-size:9px;">
-                mibnusinaga (linkedin)
-                </p><br>'''
-        )
-    ).add_to(my_map)
-
-
-
-folium.map.Marker(
-    [5.533068, 86.844171],
-    icon=DivIcon(
-        icon_size=(380,200),
-        icon_anchor=(0,0),
-        html=
-                '''<p style="font-family:Georgia Pro Regular;font-size:10px;">
+                '''<p style="font-family:Georgia Pro Regular;font-size:10px;color:white;">
                 Imigrasi Pekerja Migran Indonesia (PMI) yang ilegal ke wilayah negara Malaysia dan Singapura
                 ataupun sebaliknya lewat jalur laut, kerap kali mengalami insiden tenggelamnya kapal akibat 
                 ketidaklayakan kapal dan menempuh jalur perairan berbahaya demi menghindari petugas.
+                Visualisasi ini menunjukkan semua insiden yang mengakibatkan PMI 
+                meninggal, dengan data yang dikumpulkan berasal dari sampel berita publik Indonesia (Januari 2008-Oktober 2022).
                 </p><br>'''
         )
     ).add_to(my_map)
 
+# text metodologi
 
-folium.map.Marker(
-    [4.033068, 86.844171],
-    icon=DivIcon(
-        icon_size=(380,200),
-        icon_anchor=(0,0),
-        html=
 
-                '''<p style="font-family:Georgia Pro Regular;font-size:10px;">Visualisasi ini menunjukkan semua insiden
-                tenggelamnya kapal pengangkut pekerja migran Indonesia mulai dari Januari 2009 hingga Oktober 2022.
-                Ukuran lingkaran berwarna merah menunjukkan seberapa besar jumlah korban meninggal akibat insiden tersebut.
-                Arahkan kursor pada lingkaran dan klik untuk mengetahui informasi lengkap mengenai setiap insiden.
-                Setiap insiden kapal tenggelam 
-                didata lokasi rinci perairan dan ditentukan wilayah perairan besarnya.
-                Berdasarkan pendataan terdapat dua wilayah perairan yang umumnya terjadi kecelakaan,  
-                yaitu perairan Selat Malaka dan Laut Natuna yang telah diproyeksikan melalui pembatas segitiga bergaris putih.
-                </p><br>'''
-        )
-    ).add_to(my_map)
-folium.map.Marker(
-    [0.373068, 86.844171],
-    icon=DivIcon(
-        icon_size=(300,200),
-        icon_anchor=(0,0),
-        html=
-
-                '''<p style="font-family:Georgia Pro Regular;color:silver;font-size:7px;">
-                Metodologi : <br>                                                                            
-                Data dikumpulkan dari sampel berita publik yang berbasis di Indonesia (Januari 2009-Oktober 2022),
-                melalui proses ekstraksi dengan teknik web scraping. Untuk menjaga akurasi, data 
-                dicocokkan kembali pada laman berita secara manual.
-                </p><br>'''
-        )
-    ).add_to(my_map)
-
+# text perairan
 folium.map.Marker(
     [5.950124, 98.223917],
     icon=DivIcon(
@@ -167,45 +136,41 @@ folium.map.Marker(
         )
     ).add_to(my_map)
 
-# Adding line
+
+# membuat garis perairan
 folium.PolyLine(locations = [[5.546413, 96.075917],[5.635349, 100.049955]],
-                             popup = 'rute sama',
                              color = 'White',
-                             weight = 1,
+                             weight = 0.5,
                              opacity=1).add_to(my_map)
 folium.PolyLine(locations = [[5.546413, 96.075917],[0.543961, 103.919291]],
-                             popup = 'rute sama',
                              color = 'White',
-                             weight = 1,
+                             weight = 0.5,
                              opacity=1).add_to(my_map)
 folium.PolyLine(locations = [[5.635349, 100.049955],[0.543961, 103.919291]],
-                             popup = 'rute sama',
                              color = 'White',
-                             weight = 1,
+                             weight = 0.5,
                              opacity=1).add_to(my_map)
 folium.PolyLine(locations = [[2.696597, 104.218442],[0.761225, 105.547874]],
-                             popup = 'rute sama',
                              color = 'White',
-                             weight = 1,
+                             weight = 0.5,
                              opacity=1).add_to(my_map)
 folium.PolyLine(locations = [[2.696597, 104.218442],[0.543961, 103.919291]],
-                             popup = 'rute sama',
                              color = 'White',
-                             weight = 1,
+                             weight = 0.5,
                              opacity=1).add_to(my_map)
 folium.PolyLine(locations = [[0.543961, 103.919291],[0.761225, 105.547874]],
-                             popup = 'rute sama',
                              color = 'White',
-                             weight = 1,
+                             weight = 0.5,
                              opacity=1).add_to(my_map)
 
 
 
-# Adding Custom Markers dengan iterating city, lat,lng
-for lat, lon, death, posisi, alive, sumber, cause, tgl, laut in zip(df['lat'], df['lon'], 
+# Adding Custom Markers 
+for lat, lon, death, posisi, alive, sumber, cause, tgl, laut, link in zip(df['lat'], df['lon'], 
                                                   df['jumlah_meninggal'], df['posisi_karam'],
                                                   df['jumlah_selamat'], df['sumber'],
-                                                  df['penyebab'], df['tanggal'], df['perairan_besar']):
+                                                  df['penyebab'], df['tanggal'], df['perairan_besar'],
+                                                  df['link']):
     folium.CircleMarker(location=[lat,lon],
                   weight=1,
                   radius=death*0.3,
@@ -217,7 +182,8 @@ for lat, lon, death, posisi, alive, sumber, cause, tgl, laut in zip(df['lat'], d
                                      " orang meninggal dunia dan "+"<b>"+str(alive)+"</b>"+" orang selamat, "+
                                      "sementara penyebab kecelakaan "+str(cause)+".<br>"+
                                      "<br></b>"+
-                                     "sumber :&nbsp"+sumber,
+                                     "sumber :&nbsp"+
+                                     "<a href="+link+">"+sumber+"</a>",
                                      max_width=len(f"name= {posisi}")*10),
                   color='crimson', 
                   fill=True).add_to(my_map)
